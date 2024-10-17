b284ce00145bf006b86230d1e3a7765d5497ebf8
Pablo Rivas <pablo@rivas.com>
2024-10-17 09:21:53 +0200

    feat: add shadcn and change projects folder

---
+++ apps/client/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="p-6">
+      <Button></Button>Click me
+    </div>
+  );
+}
+
+export default App;
